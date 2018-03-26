2c8cc3850a7c2de4275d8e54ab3ae697ab60d05b
KiloMikeCodesStuff <kevin.mallinson@outlook.com>
2018-03-27 09:38:24 +1100

    Attempted to introduce tests to Cmake

--- test/src/tests.cpp
+++ test/src/tests.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "../src/factorial.cpp"
+#include "../../src/factorial.cpp"
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(1) == 1 );
